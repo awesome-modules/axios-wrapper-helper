e6b304c76a24922030000b9e78ef533218be1ac6
Sanketmundada <mundadasanket2000@gmail.com>
2021-07-24 18:34:57 +0530

    initial release

---
+++ README.md
@@ -0,0 +1,1 @@
+# axios-helper-package
---
+++ src/index.ts
@@ -0,0 +1,53 @@
+import axios, { AxiosRequestConfig, AxiosResponse } from "axios";
+
+export type ApiMethodType = "get" | "post" | "put" | "delete";
+export type GeneralConfigType = {
+  baseDomain: string;
+  tokenHeaderKey?: string;
+};
+
+let general_config: GeneralConfigType = { baseDomain: "" };
+
+const axios_config = (
+  api: string,
+  paramsOrObject: Object | null,
+  method: ApiMethodType,
+  baseDomain: string
+): AxiosRequestConfig => ({
+  method,
+  url: baseDomain + api,
+  [method === "post" ? "data" : "params"]: paramsOrObject,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+export const set_general_config = (
+  baseDomain: string,
+  tokenHeaderKey?: string
+) => {
+  general_config = { baseDomain, tokenHeaderKey };
+};
+
+export const async_func_data = async <T = any>(
+  api: string,
+  paramsOrObject: Object | null,
+  method: ApiMethodType,
+  isTokenRequired: boolean = false,
+  token?: string
+): Promise<AxiosResponse<T>> => {
+  const { baseDomain, tokenHeaderKey } = general_config;
+  try {
+    if (isTokenRequired) {
+      if (!token) throw Error("[ERR]: Token needs to be specified");
+      if (!tokenHeaderKey) throw Error("[ERR]: Token header to be specified");
+      axios.defaults.headers[tokenHeaderKey] = token;
+    }
+    const axiosConfig = axios_config(api, paramsOrObject, method, baseDomain);
+
+    const response = await axios(axiosConfig);
+    return response;
+  } catch (err) {
+    throw err.response;
+  }
+};
